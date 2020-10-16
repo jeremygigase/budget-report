0c5749bce4dfb66d46c7c3fd6597d198a6e6484b
Jeremy Gigase <jeremy.gigase@gmail.com>
2020-10-16 15:05:01 +0200

    Updates

---
+++ precache-manifest.1146ee3da5fb91f9948f32f5ff8f47ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cba8c6ac076116f6fd88ee40b0d47a18",
+    "url": "/budget-report/index.html"
+  },
+  {
+    "revision": "68621709c913854843a6",
+    "url": "/budget-report/static/css/main.c9180692.chunk.css"
+  },
+  {
+    "revision": "f771314344f03f474759",
+    "url": "/budget-report/static/js/2.56e77ece.chunk.js"
+  },
+  {
+    "revision": "f3e91ced0874a3e507ef0e80f41dde1e",
+    "url": "/budget-report/static/js/2.56e77ece.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68621709c913854843a6",
+    "url": "/budget-report/static/js/main.68e15e0e.chunk.js"
+  },
+  {
+    "revision": "4420b72d4929c7b9cb97",
+    "url": "/budget-report/static/js/runtime-main.3963bd19.js"
+  }
+]);
